d00fe80cbcf3e13f04a428c14d7f27a3c837716e
Angel David Machado <amachadoa@unal.edu.co>
2022-06-20 19:17:20 -0500

    cuarta pregunta

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/ python3
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+
+        #
+        # escribe al flujo estandar de salida
+        #
+        sys.stdout.write("{}\t1\n".format(
+            line.split('   ')[0]))
