44a449d2d7de128575173584d919ae963ce1c6d4
Angel David Machado <amachadoa@unal.edu.co>
2022-06-20 19:51:30 -0500

    septima pregunta

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/ python3
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+
+        #
+        # escribe al flujo estandar de salida
+        #
+        sys.stdout.write("{}\t{}\t{}".format(line.split('   ')[
+                         0], line.split('   ')[1], line.split('   ')[2].replace('\t', '')))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin python3
+
+import sys
+import operator
+
+orden = []
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+        key, date, val = line.split("\t")
+        val = int(val)
+        orden.append((key, date, val))
+
+    orden = sorted(orden, key=operator.itemgetter(0, 2))
+
+    for line in orden:
+        sys.stdout.write("{}\t{}\t{}\n".format(line[0], line[1], line[2]))
